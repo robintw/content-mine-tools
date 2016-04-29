120be5b732efb82f163eb3f4b4f8403e47d5b990
Robin Wilson <robin@rtwilson.com>
2016-04-29 19:25:48 +0000

    Fixed various bugs in metadata extraction

--- PyCMLib/metadata.py
+++ PyCMLib/metadata.py
@@ -7,14 +7,14 @@ def get_metadata_from_schol_html(path):
     if (path / 'results.json').exists():
         return
 
-    doc = parse_html(path / 'scholarly.html')
+    doc = parse_html(str(path / 'scholarly.html'))
 
     metadata = {}
 
-    metadata['title']['value'] = [all_whitespace_to_space(get_text_from_selector(doc, '.title-group')[0])]
-    metadata['doi']['value'] = [all_whitespace_to_space(get_text_from_selector(doc, '.doi')[0]).replace('doi: ', '')]
-    metadata['date']['value'] = ['']
-    metadata['journal']['value'] = ['']
+    metadata['title'] = {'value': [all_whitespace_to_space(get_text_from_selector(doc, '.title-group')[0])]}
+    metadata['doi'] = {'value': [all_whitespace_to_space(get_text_from_selector(doc, '.doi')[0]).replace('doi: ', '')]}
+    metadata['date'] = {'value': ['']}
+    metadata['journal'] = {'value': ['']}
 
-    with open(path / 'results.json', 'w') as f:
+    with open(str(path / 'results.json'), 'w') as f:
         json.dump(metadata, f, indent=4)
